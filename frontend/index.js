a6eb81c64ef22d6f0018a9beac05555678934a3e
Ashish kumar palai <ashishkumarpalai2000@gmail.com>
2023-07-05 19:51:05 +0530

    sayeri generator app

---
+++ frontend/index.js
@@ -0,0 +1,24 @@
+async function generateSayeri() {
+    const keywordInput = document.getElementById("keyword");
+    const keyword = keywordInput.value;
+  
+    if (!keyword) {
+      alert("Please enter a keyword");
+      return;
+    }
+  
+    const sayeriContainer = document.getElementById("sayeri");
+    const loader = document.createElement("div");
+    loader.className = "loader";
+    sayeriContainer.innerHTML = "";
+    sayeriContainer.appendChild(loader);
+  
+    try {
+      const response = await fetch(`http://localhost:8080/sayeri?keyword=${keyword}`);
+    //   http://localhost:8080/sayeri?keyword=love
+      const dataReceived = await response.json();
+      sayeriContainer.textContent = dataReceived.sayeri;
+    } catch (error) {
+        sayeriContainer.textContent = "Failed to fetch sayeri. Please try again later.";
+    }
+  }
